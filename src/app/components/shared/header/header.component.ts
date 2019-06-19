ee83fbbf2487ad208a048b412799a51841065d84
Crissavino <savinocristian89@gmail.com>
2019-06-19 11:29:18 -0300

    Subo para mostrar ultimo

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -14,17 +14,19 @@ export class HeaderComponent implements OnInit, DoCheck {
   cantidadDeProd: number;
 
   constructor(private productosService: ProductosService) { 
-    let productosCarrito: any[] = [];
-    this.productosService.getCarrito(localStorage.getItem('userId')).subscribe((carrito: any) => {
-      carrito.forEach((elemento: any) => {
-        this.productosService.getProducto(elemento.producto_id).subscribe(producto => {
-          productosCarrito.push(producto);
+    if(localStorage.getItem('userId')){
+      let productosCarrito: any[] = [];
+      this.productosService.getCarrito(localStorage.getItem('userId')).subscribe((carrito: any) => {
+        carrito.forEach((elemento: any) => {
+          this.productosService.getProducto(elemento.producto_id).subscribe(producto => {
+            productosCarrito.push(producto);
+          });
         });
       });
-    });
-    setTimeout(() => {
-      this.cantidadDeProd = productosCarrito.length;
-    }, 1000);
+      setTimeout(() => {
+        this.cantidadDeProd = productosCarrito.length;
+      }, 1000);
+    }
   }
 
   ngDoCheck() {

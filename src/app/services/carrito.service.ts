8bee41ce886c1ad2a258ff84e7c697e2b7afb3f8
Crissavino <savinocristian89@gmail.com>
2019-06-27 18:59:41 -0300

    Subo ultimo commit para mostrar v24 MercadoPago

--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -5,20 +5,20 @@ import { map, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CarritoService {
   // carritoJson = [];
   cantidadProdCarrito = 0;
-  // private urlAPI = 'http://genovevabe.cf/api';
-  private urlAPI = 'http://127.0.0.1:8000/api';
+  private urlAPI = "https://genovevabe.cf/api";
+  // private urlAPI = 'http://127.0.0.1:8000/api';
 
   constructor(private http: HttpClient) {}
 
   guardarProductoCarrito(carrito: Carrito) {
-    if (localStorage.getItem('carritoDeCompras')) {
+    if (localStorage.getItem("carritoDeCompras")) {
       const carritoDeComprasJson = JSON.parse(
-        localStorage.getItem('carritoDeCompras')
+        localStorage.getItem("carritoDeCompras")
       );
 
       carritoDeComprasJson.push({
@@ -30,11 +30,11 @@ export class CarritoService {
         cantidad: carrito.cantidad
       });
 
-      localStorage.removeItem('carritoDeCompras');
+      localStorage.removeItem("carritoDeCompras");
 
       const carritoDeComprasString = JSON.stringify(carritoDeComprasJson);
 
-      localStorage.setItem('carritoDeCompras', carritoDeComprasString);
+      localStorage.setItem("carritoDeCompras", carritoDeComprasString);
     } else {
       const carritoJson = [
         {
@@ -49,7 +49,7 @@ export class CarritoService {
 
       const carritoDeComprasString = JSON.stringify(carritoJson);
 
-      localStorage.setItem('carritoDeCompras', carritoDeComprasString);
+      localStorage.setItem("carritoDeCompras", carritoDeComprasString);
     }
   }
 
@@ -58,7 +58,7 @@ export class CarritoService {
 
     const body = JSON.stringify(carrito);
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     });
 
     return this.http.post(url, body, { headers }).pipe(
@@ -72,7 +72,7 @@ export class CarritoService {
     const url = `${this.urlAPI}/deleteCarrito/${idCarrito}`;
 
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     });
 
     return this.http.delete(url, { headers }).pipe(
@@ -86,49 +86,51 @@ export class CarritoService {
     const url = `${this.urlAPI}/getCarrito/${userId}`;
 
     let carritoDeComprasJson = [];
-    this.http.get(url).pipe().subscribe( (arregloCarrito: any) => {
-      if (arregloCarrito.length !== 0) {
-        if (arregloCarrito.length === 1) {
-          carritoDeComprasJson = [
-            {
-              id: arregloCarrito[0].id,
-              productId: arregloCarrito[0].producto_id,
-              userId: arregloCarrito[0].user_id,
-              talle: arregloCarrito[0].talle,
-              cantidad: arregloCarrito[0].cantidad
-            }
-          ];
-          const carritoDeComprasString = JSON.stringify(carritoDeComprasJson);
-          localStorage.setItem('carritoDeCompras', carritoDeComprasString);
-        } else {
-          arregloCarrito.forEach(carrito => {
-            carritoDeComprasJson.push({
-              id: carrito.id,
-              productId: carrito.producto_id,
-              userId: carrito.user_id,
-              talle: carrito.talle,
-              cantidad: carrito.cantidad
+    this.http
+      .get(url)
+      .pipe()
+      .subscribe((arregloCarrito: any) => {
+        if (arregloCarrito.length !== 0) {
+          if (arregloCarrito.length === 1) {
+            carritoDeComprasJson = [
+              {
+                id: arregloCarrito[0].id,
+                productId: arregloCarrito[0].producto_id,
+                userId: arregloCarrito[0].user_id,
+                talle: arregloCarrito[0].talle,
+                cantidad: arregloCarrito[0].cantidad
+              }
+            ];
+            const carritoDeComprasString = JSON.stringify(carritoDeComprasJson);
+            localStorage.setItem("carritoDeCompras", carritoDeComprasString);
+          } else {
+            arregloCarrito.forEach(carrito => {
+              carritoDeComprasJson.push({
+                id: carrito.id,
+                productId: carrito.producto_id,
+                userId: carrito.user_id,
+                talle: carrito.talle,
+                cantidad: carrito.cantidad
+              });
             });
-          });
 
-          const carritoDeComprasString = JSON.stringify(carritoDeComprasJson);
-          localStorage.setItem('carritoDeCompras', carritoDeComprasString);
+            const carritoDeComprasString = JSON.stringify(carritoDeComprasJson);
+            localStorage.setItem("carritoDeCompras", carritoDeComprasString);
+          }
         }
-
-      }
-    });
+      });
     return carritoDeComprasJson;
   }
 
   getCarrito() {
     const carritoDeComprasJsonUsuario = [];
-    if (localStorage.getItem('carritoDeCompras')) {
+    if (localStorage.getItem("carritoDeCompras")) {
       const carritoDeComprasJson = JSON.parse(
-        localStorage.getItem('carritoDeCompras')
+        localStorage.getItem("carritoDeCompras")
       );
 
       carritoDeComprasJson.forEach(carrito => {
-        if (carrito.userId == localStorage.getItem('userId')) {
+        if (carrito.userId == localStorage.getItem("userId")) {
           carritoDeComprasJsonUsuario.push(carrito);
         }
       });
@@ -138,7 +140,7 @@ export class CarritoService {
 
   deleteProductoCarrito(idCarritoAborrar) {
     const carritoDeComprasJson = JSON.parse(
-      localStorage.getItem('carritoDeCompras')
+      localStorage.getItem("carritoDeCompras")
     );
     carritoDeComprasJson.forEach((productoCarrito: any, index) => {
       if (productoCarrito.id === idCarritoAborrar) {
@@ -149,13 +151,13 @@ export class CarritoService {
 
     const carritoDeComprasString = JSON.stringify(carritoDeComprasJson);
     // console.log(carritoDeComprasString);
-    localStorage.removeItem('carritoDeCompras');
+    localStorage.removeItem("carritoDeCompras");
     // console.log(carritoDeComprasString);
 
-    localStorage.setItem('carritoDeCompras', carritoDeComprasString);
+    localStorage.setItem("carritoDeCompras", carritoDeComprasString);
 
     if (carritoDeComprasJson.length === 0) {
-      localStorage.removeItem('carritoDeCompras');
+      localStorage.removeItem("carritoDeCompras");
     }
   }
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,81 +8,119 @@ import { Observable, Subject } from 'rxjs';
 import { Carrito } from '../models/carrito.models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ProductosService {
-  // private urlAPI = 'http://genovevaok.com/api';
-  // private urlAPI = 'http://genovevabe.cf/api';
-  private urlAPI = 'http://127.0.0.1:8000/api';
+  private urlAPI = "https://genovevabe.cf/api";
+  // private urlAPI = 'http://127.0.0.1:8000/api';
   // para compartir data
   private terminoBuscado$ = new Subject();
 
   constructor(private http: HttpClient) {
     // const url = `${this.urlAPI}/productos`;
 
-    this.http.get(`${this.urlAPI}/productos`).pipe().subscribe( productos => {
-      const todosLosProductos = JSON.stringify(productos);
-      localStorage.setItem('todosLosProductos', todosLosProductos);
-    });
+    this.http
+      .get(`${this.urlAPI}/productos`)
+      .pipe()
+      .subscribe(productos => {
+        const todosLosProductos = JSON.stringify(productos);
+        localStorage.setItem("todosLosProductos", todosLosProductos);
+      });
 
-    this.http.get(`${this.urlAPI}/datos`).pipe().subscribe( datos => {
-      const todosLosDatos = JSON.stringify(datos);
-      localStorage.setItem('todosLosDatos', todosLosDatos);
-    });
+    this.http
+      .get(`${this.urlAPI}/datos`)
+      .pipe()
+      .subscribe(datos => {
+        const todosLosDatos = JSON.stringify(datos);
+        localStorage.setItem("todosLosDatos", todosLosDatos);
+      });
 
-    this.http.get(`${this.urlAPI}/imagenesShop`).pipe().subscribe( imagenesShop => {
-      const todosLasImagenesShop = JSON.stringify(imagenesShop);
-      localStorage.setItem('todosLasImagenesShop', todosLasImagenesShop);
-    });
+    this.http
+      .get(`${this.urlAPI}/imagenesShop`)
+      .pipe()
+      .subscribe(imagenesShop => {
+        const todosLasImagenesShop = JSON.stringify(imagenesShop);
+        localStorage.setItem("todosLasImagenesShop", todosLasImagenesShop);
+      });
 
-    this.http.get(`${this.urlAPI}/imagenesDetalle`).pipe().subscribe( imagenesDetalle => {
-      const todosLasImagenesDetalle = JSON.stringify(imagenesDetalle);
-      localStorage.setItem('todosLasImagenesDetalle', todosLasImagenesDetalle);
-    });
+    this.http
+      .get(`${this.urlAPI}/imagenesDetalle`)
+      .pipe()
+      .subscribe(imagenesDetalle => {
+        const todosLasImagenesDetalle = JSON.stringify(imagenesDetalle);
+        localStorage.setItem(
+          "todosLasImagenesDetalle",
+          todosLasImagenesDetalle
+        );
+      });
 
-    this.http.get(`${this.urlAPI}/productos/stock`).pipe().subscribe( stock => {
-      const todoElStock = JSON.stringify(stock);
-      localStorage.setItem('todoElStock', todoElStock);
-    });
+    this.http
+      .get(`${this.urlAPI}/productos/stock`)
+      .pipe()
+      .subscribe(stock => {
+        const todoElStock = JSON.stringify(stock);
+        localStorage.setItem("todoElStock", todoElStock);
+      });
 
-    this.http.get(`${this.urlAPI}/relcolores`).pipe().subscribe(relColores => {
-      const todasRelColores = JSON.stringify(relColores);
-      localStorage.setItem('todasRelColores', todasRelColores);
-    });
+    this.http
+      .get(`${this.urlAPI}/relcolores`)
+      .pipe()
+      .subscribe(relColores => {
+        const todasRelColores = JSON.stringify(relColores);
+        localStorage.setItem("todasRelColores", todasRelColores);
+      });
 
-    if (localStorage.getItem('userId')) {
-      if (!localStorage.getItem('favoritosUsuario')) {
-        this.http.get(`${this.urlAPI}/getProductosFavoritos/${localStorage.getItem('userId')}`).pipe().subscribe((favoritos: any) => {
-          if (favoritos) {
-            let favoritosUsuarioJson = [];
-            if (favoritos.length === 1) {
-              favoritosUsuarioJson = [{
-                id: favoritos.id,
-                productId: favoritos.producto_id,
-                userId: favoritos.userId
-              }];
-              const favoritosUsuarioString = JSON.stringify(favoritosUsuarioJson);
-              localStorage.setItem('favoritosUsuario', favoritosUsuarioString);
-            } else {
-              favoritos.forEach(fav => {
-                favoritosUsuarioJson.push({
-                  id: fav.id,
-                  productId: fav.producto_id,
-                  userId: fav.user_id
+    if (localStorage.getItem("userId")) {
+      if (!localStorage.getItem("favoritosUsuario")) {
+        this.http
+          .get(
+            `${this.urlAPI}/getProductosFavoritos/${localStorage.getItem(
+              "userId"
+            )}`
+          )
+          .pipe()
+          .subscribe((favoritos: any) => {
+            if (favoritos) {
+              let favoritosUsuarioJson = [];
+              if (favoritos.length === 1) {
+                favoritosUsuarioJson = [
+                  {
+                    id: favoritos.id,
+                    productId: favoritos.producto_id,
+                    userId: favoritos.userId
+                  }
+                ];
+                const favoritosUsuarioString = JSON.stringify(
+                  favoritosUsuarioJson
+                );
+                localStorage.setItem(
+                  "favoritosUsuario",
+                  favoritosUsuarioString
+                );
+              } else {
+                favoritos.forEach(fav => {
+                  favoritosUsuarioJson.push({
+                    id: fav.id,
+                    productId: fav.producto_id,
+                    userId: fav.user_id
+                  });
                 });
-              });
+              }
+              const favoritosUsuarioString = JSON.stringify(
+                favoritosUsuarioJson
+              );
+              localStorage.setItem("favoritosUsuario", favoritosUsuarioString);
             }
-            const favoritosUsuarioString = JSON.stringify(favoritosUsuarioJson);
-            localStorage.setItem('favoritosUsuario', favoritosUsuarioString);
-          }
-        });
+          });
       }
     }
   }
 
   productosDestacados() {
-    if (localStorage.getItem('todosLosProductos')) {
-      const todosLosProductos = JSON.parse(localStorage.getItem('todosLosProductos'));
+    if (localStorage.getItem("todosLosProductos")) {
+      const todosLosProductos = JSON.parse(
+        localStorage.getItem("todosLosProductos")
+      );
 
       const productosPopulares = [];
 
@@ -96,12 +134,13 @@ export class ProductosService {
     }
   }
 
-
   imagenesDetalle(idProducto) {
     let todosLasImagenesDetalleJson;
     const path = [];
-    if (localStorage.getItem('todosLasImagenesDetalle')) {
-      todosLasImagenesDetalleJson = JSON.parse(localStorage.getItem('todosLasImagenesDetalle'));
+    if (localStorage.getItem("todosLasImagenesDetalle")) {
+      todosLasImagenesDetalleJson = JSON.parse(
+        localStorage.getItem("todosLasImagenesDetalle")
+      );
 
       todosLasImagenesDetalleJson.forEach(imagen => {
         if (imagen.producto_id == idProducto) {
@@ -118,12 +157,14 @@ export class ProductosService {
     let talles;
     const stock = [];
 
-    if (localStorage.getItem('todosLosDatos')) {
-      const todosLosDatosJSon = JSON.parse(localStorage.getItem('todosLosDatos'));
+    if (localStorage.getItem("todosLosDatos")) {
+      const todosLosDatosJSon = JSON.parse(
+        localStorage.getItem("todosLosDatos")
+      );
       talles = todosLosDatosJSon.talles;
     }
-    if (localStorage.getItem('todoElStock')) {
-      todoElStockJson = JSON.parse(localStorage.getItem('todoElStock'));
+    if (localStorage.getItem("todoElStock")) {
+      todoElStockJson = JSON.parse(localStorage.getItem("todoElStock"));
 
       todoElStockJson.forEach(stockProdcuto => {
         talles.forEach(talle => {
@@ -144,11 +185,11 @@ export class ProductosService {
   }
   cargarScript(scriptUrl: string) {
     return new Promise(resolve => {
-      const scriptElement = document.createElement('script');
+      const scriptElement = document.createElement("script");
       scriptElement.src = scriptUrl;
-      scriptElement.type = 'text/javascript';
+      scriptElement.type = "text/javascript";
       scriptElement.async = true;
-      scriptElement.charset = 'utf-8';
+      scriptElement.charset = "utf-8";
       scriptElement.onload = resolve;
       const ultimo = document.body.lastChild;
       document.body.insertBefore(scriptElement, ultimo);
@@ -158,7 +199,7 @@ export class ProductosService {
 
   cargarScriptHead(scriptUrl: string) {
     return new Promise(resolve => {
-      const scriptElement = document.createElement('script');
+      const scriptElement = document.createElement("script");
       scriptElement.src = scriptUrl;
       const ultimo = document.head.lastChild;
       document.head.insertBefore(scriptElement, ultimo);
@@ -166,8 +207,8 @@ export class ProductosService {
   }
 
   borrarScript(scriptUrl: string) {
-    const url = 'http://localhost:4200/';
-    const arreglo = document.body.getElementsByTagName('script');
+    const url = "http://localhost:4200/";
+    const arreglo = document.body.getElementsByTagName("script");
     for (const i in arreglo) {
       if (arreglo.hasOwnProperty(i)) {
         const element = arreglo[i];
@@ -179,8 +220,8 @@ export class ProductosService {
   }
 
   borrarScriptHead(scriptUrl: string) {
-    const url = 'http://localhost:4200/';
-    const arreglo = document.head.getElementsByTagName('script');
+    const url = "http://localhost:4200/";
+    const arreglo = document.head.getElementsByTagName("script");
     for (const i in arreglo) {
       if (arreglo.hasOwnProperty(i)) {
         const element = arreglo[i];
@@ -193,9 +234,9 @@ export class ProductosService {
 
   cargarEstilos(styleUrl: string) {
     return new Promise((resolve, reject) => {
-      const styleElement = document.createElement('link');
+      const styleElement = document.createElement("link");
       styleElement.href = styleUrl;
-      styleElement.rel = 'stylesheet';
+      styleElement.rel = "stylesheet";
       styleElement.onload = resolve;
       document.head.appendChild(styleElement);
     });
@@ -212,32 +253,34 @@ export class ProductosService {
   // hasta aca
 
   guardarFavorito(prodFavorito) {
-
-    if (localStorage.getItem('favoritosUsuario')) {
-      const favoritosUsuarioJson = JSON.parse(localStorage.getItem('favoritosUsuario'));
+    if (localStorage.getItem("favoritosUsuario")) {
+      const favoritosUsuarioJson = JSON.parse(
+        localStorage.getItem("favoritosUsuario")
+      );
 
       favoritosUsuarioJson.push({
         id: prodFavorito.id,
         productId: prodFavorito.productId,
-        userId: prodFavorito.userId,
+        userId: prodFavorito.userId
       });
 
-      localStorage.removeItem('favoritosUsuario');
+      localStorage.removeItem("favoritosUsuario");
 
       const favoritosUsuarioString = JSON.stringify(favoritosUsuarioJson);
 
-      localStorage.setItem('favoritosUsuario', favoritosUsuarioString);
-
+      localStorage.setItem("favoritosUsuario", favoritosUsuarioString);
     } else {
-      const favoritosUsuarioJson = [{
-        id: prodFavorito.id,
-        productId: prodFavorito.productId,
-        userId: prodFavorito.userId,
-      }];
+      const favoritosUsuarioJson = [
+        {
+          id: prodFavorito.id,
+          productId: prodFavorito.productId,
+          userId: prodFavorito.userId
+        }
+      ];
 
       const favoritosUsuarioString = JSON.stringify(favoritosUsuarioJson);
 
-      localStorage.setItem('favoritosUsuario', favoritosUsuarioString);
+      localStorage.setItem("favoritosUsuario", favoritosUsuarioString);
     }
   }
 
@@ -246,25 +289,29 @@ export class ProductosService {
 
     const body = JSON.stringify(prodFavorito);
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     });
 
-    return this.http.post(url, body, { headers }).pipe(map( res => {
-      console.log(res);
-      return res;
-    }));
+    return this.http.post(url, body, { headers }).pipe(
+      map(res => {
+        console.log(res);
+        return res;
+      })
+    );
   }
 
   deleteFavoritoBD(prodFavoritoId) {
     const url = `${this.urlAPI}/deleteFavorito/${prodFavoritoId}`;
 
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     });
 
-    return this.http.delete(url, { headers }).pipe(map(res => {
-      return res;
-    }));
+    return this.http.delete(url, { headers }).pipe(
+      map(res => {
+        return res;
+      })
+    );
   }
 
   // getProdFavoritosBD(userId) {
@@ -287,7 +334,7 @@ export class ProductosService {
   // }
 
   // getStockProducto(id: number) {
-    //   const url = `${this.urlAPI}/producto/${id}/stock`;
+  //   const url = `${this.urlAPI}/producto/${id}/stock`;
 
   //   return this.http.get(url).pipe(
   //     map(res => {
@@ -296,28 +343,25 @@ export class ProductosService {
   //     );
   //   }
 
-
   // getImagenesDetalle(id: number) {
-    //   const url = `${this.urlAPI}/imagenesDetalle/${id}`;
+  //   const url = `${this.urlAPI}/imagenesDetalle/${id}`;
 
-    //   return this.http.get(url).pipe(
-    //     map(res => {
-    //       return res;
-    //     })
-    //   );
-    // }
+  //   return this.http.get(url).pipe(
+  //     map(res => {
+  //       return res;
+  //     })
+  //   );
+  // }
 
-    // getProductos() {
-      //   const url = `${this.urlAPI}/productos`;
-      //   // return this.http.get('http://127.0.0.1:8000/api/ejemplo').pipe( map( (res: any) => {
-        //   //   console.log(res);
-        //   //   console.log('res');
-        //   // }));
+  // getProductos() {
+  //   const url = `${this.urlAPI}/productos`;
+  //   // return this.http.get('http://127.0.0.1:8000/api/ejemplo').pipe( map( (res: any) => {
+  //   //   console.log(res);
+  //   //   console.log('res');
+  //   // }));
 
   //   return this.http.get(url).pipe();
   // }
-
-
 
   // getProductosDestacados() {
   //   const url = `${this.urlAPI}/productosdestacados`;
@@ -372,6 +416,4 @@ export class ProductosService {
   //     })
   //   );
   // }
-
-
 }

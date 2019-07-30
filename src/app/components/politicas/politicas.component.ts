528d4739351a07b0cc6bd8e5f467dfa4550837bc
Cristian Savino <cristiansavino@MacBook-Pro-de-Cristian.local>
2019-07-30 16:28:11 -0300

    corrigo fondo home

--- src/app/components/politicas/politicas.component.ts
+++ src/app/components/politicas/politicas.component.ts
@@ -8,8 +8,22 @@ import { ProductosService } from 'src/app/services/productos.service';
 })
 export class PoliticasComponent implements OnInit {
 
+  contenido = '';
+  tituloPag = '';
+
   constructor(private productosService: ProductosService) { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.contenido = "Estas son las politicas, terminos y condiciones de Genoveva Shop Online, cualquier duda que se le presente consultenos a traves de nuestro email o a nustras redes sociales"
+    this.productosService.editarMetaHead(this.contenido)
+
+    this.tituloPag = "Terminos y Condiciones"
+    this.productosService.editarTitulo(this.tituloPag);
+  }
+
+  ngOnDestroy() {
+    this.productosService.reiniciarMetaHead(this.contenido);
+    this.productosService.reiniciarTitulo(this.tituloPag);
+  }
 
 }
